ff5e1365708b5df11c43f4db2a580a4522fc43d8
ochieng <ochieng.oduor@student.moringaschool.com>
2025-04-22 11:50:53 +0300

    complete lab

--- src/components/QuestionItem.js
+++ src/components/QuestionItem.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-function QuestionItem({ question }) {
+function QuestionItem({ question, setQuestions, questions }) {
   const { id, prompt, answers, correctIndex } = question;
 
   const options = answers.map((answer, index) => (
@@ -9,17 +9,65 @@ function QuestionItem({ question }) {
     </option>
   ));
 
+  const handleCorrectAnswerChange = (e) => {
+    const newCorrectIndex = parseInt(e.target.value, 10); // Explicit conversion to int
+
+    fetch(`http://localhost:4000/questions/${id}`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        correctIndex: newCorrectIndex,
+      }),
+    })
+      .then((resp) => resp.json())
+      .then((data) => {
+        setQuestions((prevQuestions) => {
+          const newQuestions = prevQuestions.map((questSus) => {
+            if (questSus.id === id) {
+              return { ...questSus, correctIndex: newCorrectIndex };
+            } else {
+              return questSus;
+            }
+          });
+          return newQuestions;
+        });
+        console.log(data); // Log server response if needed
+      });
+  };
+
+  const handleDelete = () => {
+    fetch(`http://localhost:4000/questions/${id}`, {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    })
+      .then((resp) => resp.json())
+      .then((data) => {
+        setQuestions((prevQuestions) => {
+          const remainingQuestions = prevQuestions.filter((quest) => quest.id !== id);
+          return remainingQuestions;
+        });
+        console.log(data); // Log server response if needed
+      });
+  };
+
   return (
     <li>
       <h4>Question {id}</h4>
       <h5>Prompt: {prompt}</h5>
       <label>
         Correct Answer:
-        <select defaultValue={correctIndex}>{options}</select>
+        <select defaultValue={correctIndex} onChange={handleCorrectAnswerChange}>
+          {options}
+        </select>
       </label>
-      <button>Delete Question</button>
+      <button onClick={handleDelete}>Delete Question</button>
     </li>
   );
 }
 
 export default QuestionItem;
+
